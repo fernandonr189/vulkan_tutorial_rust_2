360e5d44d5da211ac635a75a76630f763ff71fb2
Fernando Navarrete <fernandonr189@outlook.com>
2025-04-16 17:00:24 -0600

    Completed base code

--- crates/glfw-bindings/src/lib.rs
+++ crates/glfw-bindings/src/lib.rs
@@ -1,14 +1,71 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(improper_ctypes)]
+
+use std::{ffi::CString, ptr::null_mut};
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub fn glfw_init() {
+    unsafe {
+        glfwInit();
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn glfw_window_hint(hint: u32, value: u32) {
+    unsafe {
+        glfwWindowHint(hint as i32, value as i32);
     }
+}
+
+pub fn glfw_create_window(width: u32, height: u32, title: &str) -> Result<GLFWwindow, GlfwError> {
+    let window_title = CString::new(title).unwrap();
+    unsafe {
+        let window_ptr = glfwCreateWindow(
+            width as i32,
+            height as i32,
+            window_title.as_ptr(),
+            null_mut(),
+            null_mut(),
+        );
+        if window_ptr.is_null() {
+            Err(GlfwError::CreateWindowError)
+        } else {
+            Ok(*window_ptr)
+        }
+    }
+}
+
+pub fn glfw_destroy_window(window: &mut GLFWwindow) {
+    unsafe {
+        glfwDestroyWindow(window);
+    }
+}
+
+pub fn glfw_terminate() {
+    unsafe {
+        glfwTerminate();
+    }
+}
+
+pub fn glfw_poll_events() {
+    unsafe {
+        glfwPollEvents();
+    }
+}
+
+pub fn glfw_window_should_close(window: &mut GLFWwindow) -> bool {
+    unsafe {
+        let window_should_close = glfwWindowShouldClose(window);
+        if window_should_close == 1 {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum GlfwError {
+    CreateWindowError,
 }
--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,39 @@
-pub struct App {}
+use glfw_bindings::{
+    self, GLFW_CLIENT_API, GLFW_FALSE, GLFW_NO_API, GLFW_RESIZABLE, GLFWwindow, glfw_create_window,
+    glfw_destroy_window, glfw_init, glfw_poll_events, glfw_terminate, glfw_window_hint,
+    glfw_window_should_close,
+};
 
-impl App {
-    pub fn run(self: &mut Self) {}
-
-    fn init_vulkan() {}
-
-    fn main_loop() {}
-
-    fn cleanup(self: &mut Self) {}
+#[derive(Default)]
+pub struct App {
+    window: Option<GLFWwindow>,
 }
 
-impl Default for App {
-    fn default() -> Self {
-        App {}
+impl App {
+    pub fn run(self: &mut Self) {
+        self.init_window();
+        self.main_loop();
+    }
+
+    fn init_window(self: &mut Self) {
+        glfw_init();
+        glfw_window_hint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfw_window_hint(GLFW_RESIZABLE, GLFW_FALSE);
+        self.window = match glfw_create_window(600, 800, "Vulkan") {
+            Ok(window) => Some(window),
+            Err(err) => panic!("Failed to create window: {:?}", err),
+        };
+    }
+
+    fn main_loop(self: &mut Self) {
+        while !glfw_window_should_close(&mut self.window.unwrap()) {
+            glfw_poll_events();
+        }
+    }
+
+    fn cleanup(self: &mut Self) {
+        glfw_destroy_window(&mut self.window.unwrap());
+        glfw_terminate();
     }
 }
 
